bfe9eb90e56820ce0d29789f36ed16a21bf8f38f
CoderMohitt <mohitjoshiyt@gmail.com>
2026-09-24 20:38:50 +0530

    day 26

---
+++ Day 26/object.js
@@ -0,0 +1,70 @@
+// const info = {
+//     name: "mohit joshi",
+//     course: "web dev",
+//     "some key": "hi there"
+// }
+
+// console.log(info.name)
+// console.log(info.course)
+
+// console.log(info["some key"])
+// console.log(info["course"])
+
+// info.name = "ram"
+// info.city = "delhi"
+
+// delete info.course
+
+// console.log(info)
+
+const obj = {
+    name: "saurabh",
+    marks: [12, 23, 45, 67]
+}
+
+const num = [1, 2, 3, 4]
+
+const a = num[1]
+const b = num[2]
+
+
+const arr = [
+    {name: "st1", marks: [23, 23]}, // 0
+    {name: "st2", marks: [83, 23]}, // 1
+]
+
+// let ans = arr[0]
+// let marks = ans.marks
+
+// console.log(marks[0])
+
+let ans = arr[1]
+
+// console.log(ans.name)
+
+
+
+let a1 = {
+    name: "shivam",
+    age: 23
+}
+
+let a2 = {
+    name: "sujal",
+    age: 34
+}
+
+console.log(a1.name)
+console.log(a2.name)
+
+let info = [a1, a2]
+
+ans = info[0]
+console.log(ans)
+console.log(ans.name)
+
+
+let nums = [1, 2, 3, 4]
+
+let d = nums[3]
+console.log(d)
---
+++ Day 26/script.js
@@ -0,0 +1,55 @@
+// function sum(a, b) {
+//     return a + b
+// }
+
+// const mul = (a, b) => a * b // arrow function
+
+// let ans = sum(12, 34)
+
+//fnname()
+
+// // console.log(ans)
+
+// let new_ans = mul(2, 3)
+// console.log(new_ans)
+
+// store the name of students
+
+let students = ["st1", "st2", "st3", "st4", "st5"]
+//              0,      1,      2,      3,      4
+
+// students[0] = "student1"
+// students.push("st6")
+// students.pop()
+// students.unshift("st0")
+students.shift()
+
+// console.log(students)
+
+// last  item index = length of array - 1
+// = 5 - 1 = 4
+
+const numbers = [1, 2, 3, 4, 5, 5]
+//               0, 1, 2, 3, 4, 5
+// 6 - 1 = 5
+
+// console.log(students[3])
+// console.log(numbers[30])
+
+// console.log(students.length)
+// console.log(numbers.length)
+
+// console.log(students[students.length - 3])
+
+let nums = [34, 23, 56, 12]
+let arr = ["hi", 1, 3.12, false, undefined]
+
+console.log(arr)
+
+// let ans = nums.indexOf(560)
+// let ans = nums.includes(34)
+// let ans = nums.join(" and ")
+// let ans = nums.reverse()
+let ans = nums.sort()
+
+console.log(ans)
